2370baf8e99fe7227ad6c71acaf198e8c3599888
Syafrillah Najjar <s.syfrllhnjjr@gmail.com>
2023-07-12 07:46:36 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Dashboard from './Dashboard';
-import HomePage from './HomePage';
-import AboutPage from './AboutPage';
-import ContactPage from './ContactPage';
+import TambahData from './TambahData';
+import Login from './Login';
+import Register from './register'
+import AddPersonForm from './AddPersonForm';
 import './styles.css';
 import './form.css';
 
@@ -12,30 +13,33 @@ const App = () => {
     <Router>
       <div className="app">
         <header>
-          <h1>World Animal</h1>
+          <h1>Zvier Zv</h1>
           <nav>
             <ul>
               <li>
-                <Link to="/">Home</Link>
+                <Link to="/dashboard">Home</Link>
               </li>
               <li>
-                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/tambahdata">Entry</Link>
+              </li>
+              {/* <li>
+                <Link to="/login">Login</Link>
               </li>
               <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact</Link>
-              </li>
+                <Link to="/register">Register</Link>
+              </li> */}
             </ul>
           </nav>
         </header>
         <main>
           <Routes>
-            <Route path="/" element={<HomePage />} />
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/tambahdata" element={<TambahData />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Login />} />
+
+            <Route path="/register" element={<Register />}/>
+            {/* <Route exact path="/add-person" component={AddPersonForm} /> */}
           </Routes>
         </main>
       </div>
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,48 +1,130 @@
-import React from 'react';
+// dashboard.js
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import './dashboard.css';
+import AddPersonForm from './AddPersonForm';
+import EditPersonForm from './editPersonForm';
 
 const Dashboard = () => {
+  const [data, setData] = useState([]);
+  const [editId, setEditId] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const token = localStorage.getItem('token');
+
+  const fetchData = async () => {
+    try {
+      const token = localStorage.getItem('token'); // Get the token from local storage
+      const headers = {
+        Authorization: `Bearer ${token}` // Add the token to the 'Authorization' header
+      };
+      console.log(token)
+  
+      const response = await axios.get('http://localhost:8080/persons', { headers });
+      setData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`http://localhost:8080/persons/${id}`);
+      console.log(`Delete entry with ID: ${id}`);
+      fetchData();
+    } catch (error) {
+      console.error('Error deleting person:', error);
+    }
+  };
+
+  const handleAddPerson = async (newPersonData) => {
+    try {
+      await axios.post('http://localhost:8080/persons', newPersonData);
+      console.log('Person added:', newPersonData);
+      fetchData();
+    } catch (error) {
+      console.error('Error adding person:', error);
+    }
+  };
+
+  const handleEdit = (id) => {
+    setEditId(id);
+  };
+
+  const handleUpdate = async (id, updatedPersonData) => {
+   try {
+      await axios.put(`http://localhost:8080/persons/${id}`, updatedPersonData);
+      console.log(`Person with ID ${id} updated:`, updatedPersonData);
+      setEditId(null);
+      fetchData();
+    } catch (error) {
+      console.error('Error updating person:', error);
+    }
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    window.location.href = '/login'; // Redirect to login page
+  };
+
   return (
-    <div className="dashboard-container">
-      <h2 className="dashboard-heading">Fakta Hewan</h2>
-      <div className="animal-card">
-        <div className="animal">
-          <img src="hewan1.jpg" alt="Koala" className="animal-image" />
-          <div className="animal-details">
-            <h3 className="animal-name">Koala</h3>
-            <p className="animal-description">
-            Koala adalah hewan endemik Australia dengan tubuh gemuk, bulu lebat abu-abu atau cokelat, dan kebiasaan makan daun eukaliptus. Mereka tidur sepanjang hari di pohon eukaliptus dan memiliki diet yang terdiri hampir secara eksklusif dari daun eukaliptus.
-            </p>
-            <Link to="/hewan1.jpg" className="animal-link">
-              Lihat Detail
-            </Link>
-          </div>
-        </div>
-        <div className="animal">
-          <img src="hewan2.jpg" alt="Tupai" className="animal-image" />
-          <div className="animal-details">
-            <h3 className="animal-name">Tupai</h3>
-            <p className="animal-description">
-            Tupai adalah hewan mamalia kecil dengan tubuh kecil, ekor panjang, dan bulu lebat. Beberapa jenis tupai dapat meluncur di udara dengan selaput kulit. Mereka adalah pemakan serangga, buah-buahan, biji-bijian, dan nektar.
-            </p>
-            <Link to="/hewan2.jpg" className="animal-link">
-              Lihat Detail
-            </Link>
-          </div>
-        </div>
-        <div className="animal">
-          <img src="hewan3.jpg" alt="Panda" className="animal-image" />
-          <div className="animal-details">
-            <h3 className="animal-name">Panda</h3>
-            <p className="animal-description">
-            Panda adalah hewan besar dengan bulu hitam dan putih. Panda raksasa hanya ditemukan di pegunungan China dan makanannya terutama bambu. Panda kecil memiliki bulu merah cokelat dan diet yang lebih beragam, termasuk buah-buahan, tunas, serangga, dan telur burung.
-            </p>
-            <Link to="/hewan3.jpg" className="animal-link">
-              Lihat Detail
-            </Link>
-          </div>
-        </div>
+    <div>
+      <h1>DAFTAR PERUSAHAAN BUMN</h1>
+      {/* <AddPersonForm handleAddPerson={handleAddPerson} /> */}
+      <table className="data-table">
+        <thead>
+          <tr>
+            <th>Nama Kantor</th>
+            <th>Pimpinan</th>
+            <th>Alamat</th>
+            <th>Hotline</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((entry) => (
+            <React.Fragment key={entry._id}>
+              {editId === entry._id ? (
+                <EditPersonForm
+                  id={entry._id}
+                  namakantor={entry.namakantor}
+                  pimpinan={entry.pimpinan}
+                  alamat={entry.alamat}
+                  hotline={entry.hotline}
+                  handleUpdate={handleUpdate}
+                />
+              ) : (
+                <tr>
+                  <td>{entry.namakantor}</td>
+                  <td>{entry.pimpinan}</td>
+                  <td>{entry.alamat}</td>
+                  <td>{entry.hotline}</td>
+                  <td>
+                    <button onClick={() => handleEdit(entry._id)} className="edit-button">
+                      Edit
+                    </button>
+                  </td>
+                  <td>
+                    <button onClick={() => handleDelete(entry._id)} className="delete-button">
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              )}
+            </React.Fragment>
+          ))}
+        </tbody>
+      </table>
+      <div className="button-container">
+        <button onClick={handleLogout} className="logout-button">
+          Logout
+        </button>
       </div>
     </div>
   );
